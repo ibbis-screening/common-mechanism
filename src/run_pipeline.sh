9bc547d815084782b2323611f480707e01af79e7
Nicole Wheeler <nwheeler443@gmail.com>
2021-12-09 10:42:28 +0000

    cleaning run script

--- src/run_pipeline.sh
+++ src/run_pipeline.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-export PFAMDB=./databases/
-export BLASTDB=$BLASTDB:../
-=======
 # usage: src/run_pipeline.sh test_sequences/[$name].fasta
 
 export PFAMDB=$PFAMDB:./databases
 export BLASTDB=$BLASTDB:./databases
->>>>>>> cfc60b3ce8102364e0710d1e86026fbb25e77671
 
 query=$1 # the file name
 name=${query//*\//}
@@ -29,20 +24,12 @@ python src/check_reg_path.py ${name}
 
 # benign DB scan
 hmmscan --domtblout ${name}.benign.hmmsearch benign/benign.hmm ${name}.faa &>/dev/null
-<<<<<<< HEAD
-blastn -db benign.fasta -query $query -out ${name}.benign.blastn -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -evalue 1e-5
-=======
 blastn -db benign/benign.fasta -query $query -out ${name}.benign.blastn -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -evalue 1e-5
->>>>>>> cfc60b3ce8102364e0710d1e86026fbb25e77671
 
 python src/check_benign.py ${name} ${query} # added the original file path here to fetch sequence length, can tidy this
 
 # functional characterization
 
-<<<<<<< HEAD
-# rm ${name}.reg_path_coords.csv $name.*hmmsearch $name.*blastx $name.*blastn
-=======
 python src/viz_outputs.py ${name}
 
 #rm ${name}.reg_path_coords.csv $name.*hmmsearch $name.*blastx $name.*blastn
->>>>>>> cfc60b3ce8102364e0710d1e86026fbb25e77671

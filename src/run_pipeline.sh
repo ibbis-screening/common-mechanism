cfc60b3ce8102364e0710d1e86026fbb25e77671
Nicole Wheeler <nwheeler443@gmail.com>
2021-12-09 10:18:37 +0000

    starting viz work

--- src/run_pipeline.sh
+++ src/run_pipeline.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# usage: src/run_pipeline.sh test_sequences/[$name].fasta
+
+export PFAMDB=$PFAMDB:./databases
+export BLASTDB=$BLASTDB:./databases
+
 query=$1 # the file name
 name=${query//*\//}
 name=${name//.fasta/} # the prefix detailing the name of the sequence
 
 # biorisk DB scan
 transeq $query ${name}.faa -frame 6 -clean &>/dev/null
-hmmscan --domtblout ${name}.biorisk.hmmsearch biorisk.hmm ${name}.faa &>/dev/null
+hmmscan --domtblout ${name}.biorisk.hmmsearch biorisk/biorisk.hmm ${name}.faa &>/dev/null
 python src/check_biorisk.py ${name}
 
 # taxon ID
@@ -18,11 +23,13 @@ blastx -db nr -query $query -out ${name}.nr.blastx -outfmt "7 qacc stitle sacc s
 python src/check_reg_path.py ${name}
 
 # benign DB scan
-hmmscan --domtblout ${name}.benign.hmmsearch benign.hmm ${name}.faa &>/dev/null
-blastn -db benign.fasta -query $query -out ${name}.benign.blastn -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -evalue 1e-5
+hmmscan --domtblout ${name}.benign.hmmsearch benign/benign.hmm ${name}.faa &>/dev/null
+blastn -db benign/benign.fasta -query $query -out ${name}.benign.blastn -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -evalue 1e-5
 
 python src/check_benign.py ${name} ${query} # added the original file path here to fetch sequence length, can tidy this
 
 # functional characterization
 
-rm ${name}.reg_path_coords.csv $name.*hmmsearch $name.*blastx $name.*blastn
+python src/viz_outputs.py ${name}
+
+#rm ${name}.reg_path_coords.csv $name.*hmmsearch $name.*blastx $name.*blastn

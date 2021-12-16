9dea3a61c832fc7297567fdf114109e8709d590b
Nicole Wheeler <nwheeler443@gmail.com>
2021-12-16 14:13:38 +0000

    fixed double variable

--- src/check_benign.py
+++ src/check_benign.py
@@ -36,9 +36,9 @@ if os.path.exists(sys.argv[1] + ".reg_path_coords.csv"):
         blastn = readblast(blast)
         for region in range(0, coords.shape[0]): # for each regulated pathogen region
             # look at only the hits that overlap with it
-            htrim = blastn[~((blastn['q.start'] > coords['q. end'][region]) & (blastn['q. end'] > coords['q. end'][region])) & ~((blastn['q.start'] < coords['q. start'][region]) & (blastn['q. from'] < coords['q. start'][region]))]
-            htrim['coverage'] = abs(htrim['q. end'] - htrim['q. start']) / htrim['qlen'][0]
-            if any(htrim['coverage'] > 0.90):
+            htrim2 = blastn[~((blastn['q.start'] > coords['q. end'][region]) & (blastn['q. end'] > coords['q. end'][region])) & ~((blastn['q.start'] < coords['q. start'][region]) & (blastn['q. from'] < coords['q. start'][region]))]
+            htrim2['coverage'] = abs(htrim['q. end'] - htrim['q. start']) / htrim['qlen'][0]
+            if any(htrim2['coverage'] > 0.90):
                 print("Housekeeping genes - >90% coverage achieved - PASS")
 else:
     print("No regulated regions to clear")

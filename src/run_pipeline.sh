01bcb6dd85b0d74f11f5f984b264b84bc242c407
Nicole Wheeler <nwheeler443@gmail.com>
2021-12-20 12:59:46 +0000

    packaging

--- src/run_pipeline.sh
+++ src/run_pipeline.sh
@@ -2,8 +2,10 @@
 
 # usage: src/run_pipeline.sh test_sequences/[$name].fasta
 
-export PFAMDB=./databases
-export BLASTDB=$BLASTDB:./databases
+PKG_HOME=`which run_pipeline.sh | sed 's/\/src\/run_pipeline.sh//g'`
+
+export PFAMDB=$PKG_HOME/databases
+export BLASTDB=$BLASTDB:$PKG_HOME/databases
 
 query=$1 # the file name
 name=${query//*\//}
@@ -12,7 +14,7 @@ name=${name//.fasta/} # the prefix detailing the name of the sequence
 # biorisk DB scan
 transeq $query ${name}.faa -frame 6 -clean &>/dev/null
 hmmscan --domtblout ${name}.biorisk.hmmsearch biorisk/biorisk.hmm ${name}.faa &>/dev/null
-python check_biorisk.py ${name}
+python -m check_biorisk ${name}
 
 # taxon ID
 if ! [ -e "${name}.nr.blastx" ];
@@ -22,16 +24,19 @@ then blastx -db nr -query $query -out ${name}.nr.blastx -outfmt "7 qacc stitle s
 fi
 
 ### IF A HIT TO A REGULATED PATHOGEN, PROCEED, OTHERWISE CAN FINISH HERE ONCE TESTING IS COMPLETE ####
-python check_reg_path.py ${name}
+python -m check_reg_path ${name}
 
 # benign DB scan
 hmmscan --domtblout ${name}.benign.hmmsearch benign/benign.hmm ${name}.faa &>/dev/null
 blastn -db benign/benign.fasta -query $query -out ${name}.benign.blastn -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -evalue 1e-5
 
-python check_benign.py ${name} ${query} # added the original file path here to fetch sequence length, can tidy this
+python -m check_benign ${name} ${query} # added the original file path here to fetch sequence length, can tidy this
 
 # functional characterization
 
-python viz_outputs.py ${name}
+if [ ! -d //figures ]; then
+  mkdir -p figures;
+fi
+python -m viz_outputs ${name}
 
 #rm ${query}.reg_path_coords.csv $name.*hmmsearch $name.*blastx $name.*blastn

7935b3bd5a123f89bd1bdc0fdcba6525a5ab6799
Nicole Wheeler <nwheeler443@gmail.com>
2021-12-21 17:36:40 +0000

    db path

--- src/run_pipeline.sh
+++ src/run_pipeline.sh
@@ -2,10 +2,11 @@
 
 # usage: src/run_pipeline.sh test_sequences/[$name].fasta
 
-PKG_HOME=`which run_pipeline.sh | sed 's/\/src\/run_pipeline.sh//g'`
+#PKG_HOME=`which run_pipeline.sh | sed 's/\/src\/run_pipeline.sh//g'`
 
-export PFAMDB=$PKG_HOME/databases
-export BLASTDB=$BLASTDB:$PKG_HOME/databases
+export PYTHONPATH=$PYTHONPATH/src
+export PFAMDB=./databases
+export BLASTDB=$BLASTDB:./databases
 
 query=$1 # the file name
 name=${query//*\//}
@@ -20,7 +21,7 @@ python -m check_biorisk ${name}
 if ! [ -e "${name}.nr.blastx" ];
 # then blastx -db nr -query $query -out ${name}.nr.blastx -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -max_target_seqs 500 -evalue 1e-5 -remote
 # not enough room on computer
-then blastx -db nr -query $query -out ${name}.nr.blastx -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -max_target_seqs 500 -evalue 1e-5 -num_threads 8 -culling_limit 5 -evalue 1e-10 -word_size 6 -threshold 21 -window_size 40 -matrix BLOSUM62 -gapopen 11 -gapextend 1 -seg yes
+then blastx -db nr -query $query -out ${name}.nr.blastx -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -max_target_seqs 500 -num_threads 8 -culling_limit 5 -evalue 1e-10 -word_size 6 -threshold 21 -window_size 40 -matrix BLOSUM62 -gapopen 11 -gapextend 1 -seg yes
 fi
 
 ### IF A HIT TO A REGULATED PATHOGEN, PROCEED, OTHERWISE CAN FINISH HERE ONCE TESTING IS COMPLETE ####

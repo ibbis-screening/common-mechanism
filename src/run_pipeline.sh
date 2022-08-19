6ee9ebf9e47852c51dc86e6e2bf364055c65b883
Nicole Wheeler <nwheeler443@gmail.com>
2022-08-19 13:01:38 +0100

    more verbost

--- src/run_pipeline.sh
+++ src/run_pipeline.sh
@@ -78,7 +78,7 @@ fi
 #    echo " ERROR: database folder $DB_PATH does not exist"
 #    exit
 #fi
-#  
+#
 ##Check for input file
 #if [ ! -f  $QUERY ]
 #then
@@ -100,11 +100,13 @@ name=${QUERY//*\//} # strip out any directory info
 name=${QUERY//.fasta/} # the prefix detailing the name of the sequence
 
 # Step 1: biorisk DB scan
+echo " >> Running biorisk HMM scan..."
 transeq $QUERY ${name}.faa -frame 6 -clean &>/dev/null
 hmmscan --domtblout ${name}.biorisk.hmmsearch biorisk/biorisk.hmm ${name}.faa &>/dev/null
 python -m check_biorisk ${name}
 
 # Step 2: taxon ID
+echo " >> Running taxid screen for regulated pathogens..."
 #if ! [ -e "${name}.nr.blastx" ]; # if the file already exists, don't remake it - can remove this upon release (if added back, remember then before blastx command)
 src/run_blastx.sh -d nr -q $QUERY -o ${name}.nr
 
@@ -112,10 +114,13 @@ src/run_blastx.sh -d nr -q $QUERY -o ${name}.nr
 python -m check_reg_path ${name}
 
 # Step 3: benign DB scan
+echo " >> Checking any pathogen regions for benign components..."
 hmmscan --domtblout ${name}.benign.hmmsearch benign/benign.hmm ${name}.faa &>/dev/null
 blastn -db benign/benign.fasta -query $QUERY -out ${name}.benign.blastn -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -evalue 1e-5
 
 python -m check_benign ${name} ${QUERY} # added the original file path here to fetch sequence length, can tidy this
+
+echo " >> Done with screening!"
 
 # Visualising outputs; functional characterization
 

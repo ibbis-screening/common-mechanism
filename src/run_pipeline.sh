f8700ba6c604e0b7c071c3d8f580bd2c3b717863
Nicole Wheeler <nwheeler443@gmail.com>
2022-08-19 07:38:00 +0100

    double argument

--- src/run_pipeline.sh
+++ src/run_pipeline.sh
@@ -18,7 +18,7 @@ python -m check_biorisk ${name}
 
 # Step 2: taxon ID
 if ! [ -e "${name}.nr.blastx" ]; # if the file already exists, don't remake it - can remove this upon release
-then blastx -db nr -query $query -out ${name}.nr.blastx -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -max_target_seqs 500 -num_threads 8 -culling_limit 5 -evalue 1e-10 -word_size 6 -threshold 21 -window_size 40 -matrix BLOSUM62 -gapopen 11 -num_threads 7 -gapextend 1 -seg yes
+then blastx -db nr -query $query -out ${name}.nr.blastx -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -max_target_seqs 500 -num_threads 8 -culling_limit 5 -evalue 1e-10 -word_size 6 -threshold 21 -window_size 40 -matrix BLOSUM62 -gapopen 11 -gapextend 1 -seg yes
 fi
 
 # here need to flag any uncovered regions >200 bp and blastn them

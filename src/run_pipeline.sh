b3d8e9448a7d31fac9ab80794e470e87f53d4da9
Nicole Wheeler <nwheeler443@gmail.com>
2022-08-19 13:38:54 +0100

    opts

--- src/run_pipeline.sh
+++ src/run_pipeline.sh
@@ -12,11 +12,10 @@ THREADS=1           #threads per process
 DB_PATH=""
 QUERY=""
 OUTPUT=""
-
-# to add - cleanup vs no cleanup
+CLEANUP=1
 
 #Get options from user
-while getopts "p:t:d:q:o:" OPTION
+while getopts "p:t:d:q:o:c:" OPTION
     do
         case $OPTION in
             p)
@@ -34,6 +33,9 @@ while getopts "p:t:d:q:o:" OPTION
             o)
                 OUTPUT=$OPTARG
                 ;;
+            c)
+                CLEANUP=$OPTARG
+                ;;
             \?)
                 echo "Usage: src/run_pipeline.sh -d DB_PATH -q QUERY -s OUTPUT [-p PROCESSES -t THREADS]"
                 echo "  DB_PATH              location (folder) of database (required)"
@@ -41,6 +43,7 @@ while getopts "p:t:d:q:o:" OPTION
                 echo "  OUTPUT          output prefix for alignments (default: query prefix)"
                 echo "  PROCESSES       number of databases to evaluate (default: 5)"
                 echo "  THREADS         number of threads for each database run (default: 1)"
+                echo "  CLEANUP         tidy up intermediate screening files afterward?"
                 exit
                 ;;
         esac
@@ -55,6 +58,7 @@ then
         echo "  OUTPUT          output prefix for alignments (default: out)"
         echo "  PROCESSES       number of databases to evaluate (default: 5)"
         echo "  THREADS         number of threads for each database run (default: 1)"
+        echo "  CLEANUP         tidy up intermediate screening files afterward? 0 = no, 1 = y"
     exit
 fi
 
@@ -65,26 +69,26 @@ then
 fi
 
 #Check for database
-#echo " >> Checking for valid options..."
-#if [ -d $DB_PATH ]
-#then
-#    #Directory exists, check for at least one blastx db file
-#    if [ ! -f $DB_PATH/*.phr ]
-#    then
-#        echo " ERROR: no blast database in $DB_PATH"
-#        exit
-#    fi
-#else
-#    echo " ERROR: database folder $DB_PATH does not exist"
-#    exit
-#fi
-#
-##Check for input file
-#if [ ! -f  $QUERY ]
-#then
-#    echo " ERROR: input file $QUERY does not exist"
-#    exit
-#fi
+echo " >> Checking for valid options..."
+if [ -d "$DB_PATH" ]
+then
+    #Directory exists, check for at least one blastx db file
+    if [ ! -f "$DB_PATH"/*.pal ]
+    then
+        echo " ERROR: no blast database in $DB_PATH"
+        exit
+    fi
+else
+    echo " ERROR: database folder $DB_PATH does not exist"
+    exit
+fi
+
+#Check for input file
+if [ ! -f  $QUERY ]
+then
+    echo " ERROR: input file $QUERY does not exist"
+    exit
+fi
 
 #################################################################
 
@@ -92,9 +96,9 @@ fi
 dirname="$( dirname "$0" )"
 
 export CM_DIR=$dirname
-export PYTHONPATH=$PYTHONPATH:$dirname
+export PYTHONPATH=$dirname
 
-#source ${dirname}/config
+source ${CM_DIR}/../config
 
 name=${QUERY//*\//} # strip out any directory info
 name=${QUERY//.fasta/} # the prefix detailing the name of the sequence
@@ -107,8 +111,7 @@ python -m check_biorisk ${name}
 
 # Step 2: taxon ID
 echo " >> Running taxid screen for regulated pathogens..."
-#if ! [ -e "${name}.nr.blastx" ]; # if the file already exists, don't remake it - can remove this upon release (if added back, remember then before blastx command)
-${CM_DIR}/run_blastx.sh -d ${DB_PATH}/nr -q $QUERY -o ${name}.nr
+${CM_DIR}/run_blastx.sh -d ${DB_PATH}/nr -q $QUERY -o ${name}.nr -t $THREADS
 
 ### IF A HIT TO A REGULATED PATHOGEN, PROCEED, OTHERWISE CAN FINISH HERE ONCE TESTING IS COMPLETE ####
 python -m check_reg_path ${name}
@@ -116,7 +119,7 @@ python -m check_reg_path ${name}
 # Step 3: benign DB scan
 echo " >> Checking any pathogen regions for benign components..."
 hmmscan --domtblout ${name}.benign.hmmsearch benign/benign.hmm ${name}.faa &>/dev/null
-blastn -db benign/benign.fasta -query $QUERY -out ${name}.benign.blastn -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -evalue 1e-5
+blastn -db ${PFAMDB}/benign/benign.fasta -query $QUERY -out ${name}.benign.blastn -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -evalue 1e-5
 
 python -m check_benign ${name} ${QUERY} # added the original file path here to fetch sequence length, can tidy this
 
@@ -126,5 +129,9 @@ echo " >> Done with screening!"
 
 #python -m viz_outputs ${name} # turning off until file write permissions are resolved
 
-#rm ${QUERY}.reg_path_coords.csv $name.*hmmsearch $name.*blastx $name.*blastn
+if [ "$CLEANUP" == 1 ]
+then
+    rm ${name}.reg_path_coords.csv ${name}.*hmmsearch ${name}.*blastx ${name}.*blastn
+fi
+
 rm tmp

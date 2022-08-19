5badb1635d8b02ac66fed1806461e760c6eac227
Nicole Wheeler <nwheeler443@gmail.com>
2022-08-19 07:09:10 +0100

    nt screening bug

--- src/check_reg_path.py
+++ src/check_reg_path.py
@@ -13,12 +13,14 @@ if len(sys.argv) < 1:
     sys.stdout.write("\tERROR: Please provide a query file\n")
     exit(1)
 
+query = sys.argv[1]
+
 # read in protein screening
-file = sys.argv[1] + ".nr.blastx"
+file = query + ".nr.blastx"
 reg_ids = pd.read_csv(os.environ['PFAMDB'] + '/biorisk/reg_taxids', header=None)
 
 blast = readblast(file)
-blast = taxdist(blast, reg_ids, sys.argv[1])
+blast = taxdist(blast, reg_ids, query)
 #print(blast)
 
 # trim down to the top hit for each region
@@ -39,22 +41,3 @@ if blast['regulated'].sum(): # if ANY of the hits are regulated
 else:
 	print("Regulated pathogens: PASS")
 
-# read in nt screening
-
-file = sys.argv[1] + ".nt.blastn"
-reg_ids = pd.read_csv(os.environ['PFAMDB'] + '/biorisk/reg_taxids', header=None)
-
-blast = taxdist(file, reg_ids, sys.argv[1])
-#print(blast)
-blast = trimblast(blast)
-#print(blast)
-
-if blast['regulated'].sum():
-    print("Regulated pathogens (nt): FLAG")
-    if "Viruses" in set(blast['superkingdom'][blast['regulated'] == True]):
-        print("Regulated virus (nt)")
-    hits = blast[blast['regulated']==True][['q. start', 'q. end']]   # print out the start and end coordinated on the query sequence
-#    print(hits)
-    hits.to_csv(sys.argv[1] + ".reg_path_coords_nt.csv", index=False)
-else:
-    print("Regulated pathogens (nt): PASS")

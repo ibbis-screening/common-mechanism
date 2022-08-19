ba7368368b2e1d8f7c5b3dec0616539031ec485f
Nicole Wheeler <nwheeler443@gmail.com>
2022-08-19 11:50:40 +0100

    making more modular

---
+++ src/run_blastx.sh
@@ -0,0 +1,89 @@
+#! /usr/bin/env bash
+
+#####################################################################
+#run_blastx.sh runs blastx against a specified database.
+#####################################################################
+#Usage: run_blastx.sh -d DB -q QUERY -o OUTPUT [-p PROCESSES -t THREADS -f FURTHEROPT]
+
+#set -eux #debug mode
+set -eu
+PROCESSES=5         #number of processes to run at once 
+THREADS=1           #threads per process  
+DB=""
+QUERY=""
+OUTPUT="out" 
+FURTHEROPT=""
+
+#Get options from user
+while getopts "p:t:d:q:o:f:" OPTION
+    do 
+        case $OPTION in
+            p) 
+                PROCESSES=$OPTARG
+                ;;
+            t) 
+                THREADS=$OPTARG
+                ;;
+            d) 
+                DB=$OPTARG
+                ;;
+            q)
+                QUERY=$OPTARG
+                ;;
+            o) 
+                OUTPUT=$OPTARG
+                ;;
+            f)
+                FURTHEROPT=$OPTARG
+                ;;
+            \?)
+                echo "Usage: run_blastx.sh -d DB -q QUERY -s OUTPUT [-p PROCESSES -t THREADS]"
+                echo "  DB              location (folder) of database (required)"
+                echo "  QUERY           query file to align to each database (required)"
+                echo "  OUTPUT          output prefix for alignments (default: out)"
+                echo "  PROCESSES       number of databases to evaluate (default: 5)"
+                echo "  THREADS         number of threads for each database run (default: 1)"
+                echo "  FURTHEROPT      any further options to specify"
+                exit
+                ;;  
+        esac 
+    done
+
+#Check for values
+if [ "$DB_PATH" == "" ] && [ "$INPUT" == "" ]
+then
+    echo "Usage: run_blastx.sh -d DB -q QUERY -s OUTPUT [-p PROCESSES -t THREADS]"
+        echo "  DB              location (folder) of database (required)"
+        echo "  QUERY           query file to align to each database (required)"
+        echo "  OUTPUT          output prefix for alignments (default: out)"
+        echo "  PROCESSES       number of databases to evaluate (default: 5)"
+        echo "  THREADS         number of threads for each database run (default: 1)"
+        echo "  FURTHEROPT      any further options to specify"
+    exit
+fi
+
+#Check for database
+echo " >> Checking for Valid Options..." 
+if [ -d $DB ]
+then 
+    #Directory exists, check for at least one blastx db file
+    if [ ! -f $DB/*.phr ]
+    then 
+        echo " ERROR: blastx database $DB/* does not exist"
+        exit
+    fi
+else
+    echo " ERROR: blast database folder $DB does not exist"
+    exit
+fi
+  
+#Check for input file 
+if [ ! -f  $QUERY ]
+then
+    echo " ERROR: input file $QUERY does not exist"
+    exit
+fi      
+  
+#ls ${DB}/*.phr | parallel --will-cite -j ${PROCESSES} blastx -db ${DB}/uniref90.{%}.dmnd --fast --threads ${THREADS} -q ${INPUT} -o ${OUTPUT}.{%}.tsv --frameshift 15 --range-culling --outfmt 6 qseqid stitle sseqid staxids evalue bitscore pident qlen qstart qend slen sstart send
+
+blastx -db $DB -query $QUERY -out ${OUTPUT}.blastx -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -max_target_seqs 500 -culling_limit 5 -evalue 1e-10 -word_size 6 -threshold 21 -window_size 40 -matrix BLOSUM62 -gapopen 11 -gapextend 1 -seg yes $FURTHEROPT
--- src/run_pipeline.sh
+++ src/run_pipeline.sh
@@ -5,7 +5,8 @@
 #PKG_HOME=`which run_pipeline.sh | sed 's/\/src\/run_pipeline.sh//g'`
 
 # run a config file that sets relevant paths
-source config
+dirname = "$( dirname "$0" )"
+source ${dirname}/config
 
 query=$1 # the file name
 name=${query//*\//} # strip out any directory info
@@ -17,12 +18,11 @@ hmmscan --domtblout ${name}.biorisk.hmmsearch biorisk/biorisk.hmm ${name}.faa &>
 python -m check_biorisk ${name}
 
 # Step 2: taxon ID
-if ! [ -e "${name}.nr.blastx" ]; # if the file already exists, don't remake it - can remove this upon release
-then blastx -db nr -query $query -out ${name}.nr.blastx -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -max_target_seqs 500 -num_threads 8 -culling_limit 5 -evalue 1e-10 -word_size 6 -threshold 21 -window_size 40 -matrix BLOSUM62 -gapopen 11 -gapextend 1 -seg yes
-fi
+#if ! [ -e "${name}.nr.blastx" ]; # if the file already exists, don't remake it - can remove this upon release (if added back, remember then before blastx command)
+#blastx -db nr -query $query -out ${name}.nr.blastx -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -max_target_seqs 500 -num_threads 8 -culling_limit 5 -evalue 1e-10 -word_size 6 -threshold 21 -window_size 40 -matrix BLOSUM62 -gapopen 11 -gapextend 1 -seg yes
+#fi
 
-# here need to flag any uncovered regions >200 bp and blastn them
-#currently running this as a step in check_reg_path
+src/run_blastx.sh -d nr -q $query -o ${name}.nr
 
 ### IF A HIT TO A REGULATED PATHOGEN, PROCEED, OTHERWISE CAN FINISH HERE ONCE TESTING IS COMPLETE ####
 python -m check_reg_path ${name}

686f3faff43bfb18c5c52b2640d8386fccc5aaeb
Nicole Wheeler <nwheeler443@gmail.com>
2022-09-11 18:19:41 +0100

    coordinate sorting nt

--- src/fetch_nc_bits_dmnd.py
+++ src/fetch_nc_bits_dmnd.py
@@ -4,6 +4,10 @@ from utils import *
 import os, sys
 import pandas as pd
 import taxoniq
+#from Bio import SeqIO
+#from Bio.Seq import Seq
+#from Bio.SeqRecord import SeqRecord
+#from collections import defaultdict
 import pybedtools
 
 #Input parameter error checking
@@ -13,43 +17,44 @@ if len(sys.argv) < 1:
     
 query = sys.argv[1]
 f_file = sys.argv[2]
-blast_file = query + ".nr.blastx"
+dmnd_file = query + ".nr.diamond"
 
 # find noncoding bits
-blast = readblast(blast_file)
-blast = trimblast(blast)
+diamond = readdmnd(dmnd_file)
+diamond = trimblast(diamond)
 # print(diamond)
 hits = []
-for i in range(blast.shape[0]):
-    pair = [blast['q. start'][i], blast['q. end'][i]]
+for i in range(diamond.shape[0]):
+    pair = [diamond['q. start'][i], diamond['q. end'][i]]
     pair.sort()
+    # print(pair)
     hits.append(pair)
-hits = sorted(hits, key=lambda x: x[0])
+#print(hits)
 
 nc_bits = []
-if hits[0][0] >20:
+if hits[0][0] != 1:
     nc_bits.append([1,hits[0][0]])
 for i in range(len(hits)-1):
-    if hits[i][1] < (hits[i+1][0] - 19): # if there's a gap of >=20 between hits
+    if hits[i][1] < hits[i+1][0]:
         nc_bits.append([hits[i][1], hits[i+1][0]])
 #print(nc_bits)
 
 
 # fetch noncoding sequences
 
-seqid = blast.iloc[0][0]
+query="1_biorisk"
+seqid = diamond.iloc[0][0]
 
 # nc_bits.append([2,4])
 
 tofetch = ""
 for (start, stop) in nc_bits:
-    tofetch = tofetch + str(seqid) + " " + str(start) + " " + str(stop) + "\n"
+    tofetch = tofetch + seqid + " " + str(start) + " " + str(stop) + "\n"
 
-if tofetch != "":
-    a = pybedtools.BedTool(tofetch, from_string=True)
-    fasta = f_file
-    outfile = query + '_nc.fasta'
-    a = a.sequence(fi=fasta, fo=outfile)
+a = pybedtools.BedTool(tofetch, from_string=True)
+fasta = f_file
+outfile = query + '_nc.fasta'
+a = a.sequence(fi=fasta, fo=outfile)
 # print(open(a.seqfn).read())
 
 

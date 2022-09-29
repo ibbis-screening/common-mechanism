c1c2ba91b6013daeb4eb3f9fcda24b545e03d115
Nicole Wheeler <nwheeler443@gmail.com>
2022-09-29 19:08:56 +0100

    remove exit from nt screen

--- src/check_reg_path_nt.py
+++ src/check_reg_path_nt.py
@@ -21,50 +21,49 @@ reg_ids = pd.read_csv(os.environ['PFAMDB'] + '/biorisk/reg_taxids', header=None)
 
 if checkfile(file) == 2:
     print("No nt hits found")
-    exit(1)
-
-blast = readblast(file)
-blast = taxdist(blast, reg_ids, query)
-#print(blast['regulated'])
-
-# trim down to the top hit for each region
-blast2 = trimblast(blast)
-#print(blast.iloc[:,10:17])
-
-# ignore synthetic constructs when deciding whether to flag
-
-if blast2['regulated'].sum(): # if ANY of the hits are regulated
-    print("Regulated pathogen nucleotides: PRESENT")
-    for gene in set(blast2['subject acc.'][blast2['regulated'] == True]): # for each gene with at least one regulated hit
-        # go back to blast - the full set of hits
-        # if it's a viral protein
-        subset = blast[(blast['subject tax ids'] != "32630") & (blast['subject acc.'] == gene)]
-        subset = subset.reset_index(drop=True)
-        if "Viruses" in set(subset['superkingdom']):
-            # if the top hit is both viral and regulated
-            if subset['superkingdom'][0] == "Viruses":
-                if subset['regulated'][0] == True:
-                    print("Regulated virus top hit: FLAG")
-                    reg_vir = 1
-            else:
-                print("Regulated virus in hits: COND FLAG")
-        elif subset['regulated'][0] == True: # if top hit that isn't a synthetic construct is regulated
-            print("Regulated bacteria top hit: FLAG")
-            reg_bac = 1
-        n_reg = blast['regulated'][blast['subject acc.'] == gene].sum()
-        n_total = len(blast['regulated'][blast['subject acc.'] == gene])
-        if (n_reg < n_total):
-            print("Gene " + gene + " found in both regulated and nonregulated organisms")
-            print("Species: " + " ".join(set(blast['species'][blast['subject acc.'] == gene])))
-        elif (n_reg == n_total):
-            print("Gene " + gene + " found in only regulated organisms")
-            print("Species: " + " ".join(set(blast['species'][blast['subject acc.'] == gene])) + " (taxid: " + " ".join(map(str, set(blast['subject tax ids'][blast['subject acc.'] == gene]))) + ")")
-        else:
-            print("Gene: " + gene)
-            print(blast['regulated'][blast['subject acc.'] == gene])
-#    hits = diamond[diamond['regulated']==True][['q. start', 'q. end']]   # print out the start and end coordinated on the query sequence
-    hits = blast[blast['regulated']==True]  # print out the start and end coordinated on the query sequence
-    hits.to_csv(sys.argv[1] + ".reg_path_coords.csv", index=False)
 else:
-    print("Regulated pathogen nucleotides: PASS")
+    blast = readblast(file)
+    blast = taxdist(blast, reg_ids, query)
+    #print(blast['regulated'])
+
+    # trim down to the top hit for each region
+    blast2 = trimblast(blast)
+    #print(blast.iloc[:,10:17])
+
+    # ignore synthetic constructs when deciding whether to flag
+
+    if blast2['regulated'].sum(): # if ANY of the hits are regulated
+        print("Regulated pathogen nucleotides: PRESENT")
+        for gene in set(blast2['subject acc.'][blast2['regulated'] == True]): # for each gene with at least one regulated hit
+            # go back to blast - the full set of hits
+            # if it's a viral protein
+            subset = blast[(blast['subject tax ids'] != "32630") & (blast['subject acc.'] == gene)]
+            subset = subset.reset_index(drop=True)
+            if "Viruses" in set(subset['superkingdom']):
+                # if the top hit is both viral and regulated
+                if subset['superkingdom'][0] == "Viruses":
+                    if subset['regulated'][0] == True:
+                        print("Regulated virus top hit: FLAG")
+                        reg_vir = 1
+                else:
+                    print("Regulated virus in hits: COND FLAG")
+            elif subset['regulated'][0] == True: # if top hit that isn't a synthetic construct is regulated
+                print("Regulated bacteria top hit: FLAG")
+                reg_bac = 1
+            n_reg = blast['regulated'][blast['subject acc.'] == gene].sum()
+            n_total = len(blast['regulated'][blast['subject acc.'] == gene])
+            if (n_reg < n_total):
+                print("Gene " + gene + " found in both regulated and nonregulated organisms")
+                print("Species: " + " ".join(set(blast['species'][blast['subject acc.'] == gene])))
+            elif (n_reg == n_total):
+                print("Gene " + gene + " found in only regulated organisms")
+                print("Species: " + " ".join(set(blast['species'][blast['subject acc.'] == gene])) + " (taxid: " + " ".join(map(str, set(blast['subject tax ids'][blast['subject acc.'] == gene]))) + ")")
+            else:
+                print("Gene: " + gene)
+                print(blast['regulated'][blast['subject acc.'] == gene])
+    #    hits = diamond[diamond['regulated']==True][['q. start', 'q. end']]   # print out the start and end coordinated on the query sequence
+        hits = blast[blast['regulated']==True]  # print out the start and end coordinated on the query sequence
+        hits.to_csv(sys.argv[1] + ".reg_path_coords.csv", index=False)
+    else:
+        print("Regulated pathogen nucleotides: PASS")
 

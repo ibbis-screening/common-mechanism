727b8bb84adacafc02780ec5805bb2b207d1807f
Nicole Wheeler <nwheeler443@gmail.com>
2022-10-27 11:54:24 +0200

    blast var fix

--- src/check_reg_path_dmnd_prot.py
+++ src/check_reg_path_dmnd_prot.py
@@ -48,17 +48,17 @@ if diamond2['regulated'].sum(): # if ANY of the hits are regulated
         elif subset['regulated'][0] == True: # if top hit that isn't a synthetic construct is regulated
             print("Regulated bacteria top hit: FLAG")
             reg_bac = 1
-        n_reg = blast['regulated'][blast['subject acc.'] == gene].sum()
-        n_total = len(blast['regulated'][blast['subject acc.'] == gene])
+        n_reg = diamond['regulated'][diamond['subject acc.'] == gene].sum()
+        n_total = len(diamond['regulated'][diamond['subject acc.'] == gene])
         if (n_reg < n_total):
             print("Gene " + gene + " found in both regulated and nonregulated organisms")
-            print("Species: " + " ".join(set(blast['species'][blast['subject acc.'] == gene])))
+            print("Species: " + " ".join(set(diamond['species'][diamond['subject acc.'] == gene])))
         elif (n_reg == n_total):
             print("Gene " + gene + " found in only regulated organisms")
-            print("Species: " + ", ".join(set(blast['species'][blast['subject acc.'] == gene])) + " (taxid: " + " ".join(map(str, set(blast['subject tax ids'][blast['subject acc.'] == gene]))) + ")")
+            print("Species: " + ", ".join(set(diamond['species'][diamond['subject acc.'] == gene])) + " (taxid: " + " ".join(map(str, set(diamond['subject tax ids'][diamond['subject acc.'] == gene]))) + ")")
         else:
             print("Gene: " + gene)
-            print(blast['regulated'][blast['subject acc.'] == gene])
+            print(diamond['regulated'][diamond['subject acc.'] == gene])
     hits = diamond2[diamond2['regulated']==True][['q. start', 'q. end']]  # print out the start and end coordinated on the query sequence
     hits.to_csv(sys.argv[1] + ".reg_path_coords.csv", index=False)
 

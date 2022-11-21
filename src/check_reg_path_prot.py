5d92c52ffe9fa3946c63dac0042906bec3f50cb1
Nicole Wheeler <nwheeler443@gmail.com>
2022-11-21 12:49:56 +0000

    sntandardised reporting

--- src/check_reg_path_prot.py
+++ src/check_reg_path_prot.py
@@ -33,40 +33,42 @@ reg_fung = 0
 if blast2['regulated'].sum(): # if ANY of the trimmed hits are regulated
 #    print(blast2)
     print("Regulated pathogen proteins: PRESENT")
-    for gene in set(blast2['subject acc.'][blast2['regulated'] == True]): # for each gene with at least one regulated hit
+    print(blast[['subject acc.', 'regulated', 'genus', 'species']])
+    # for each hit (subject acc) linked with at least one regulated taxid
+    for gene in set(blast2['subject acc.'][blast2['regulated'] == True]): 
         # go back to blast - the full set of hits
-        # if it's a viral protein
         subset = blast[(blast['subject acc.'] == gene)]
         subset = subset.reset_index(drop=True)
+        org = ""
+        # if the top hit is found in regulated pathogens
         if subset['regulated'][0] == True:
-            if subset['superkingdom'][0] == "Viruses":
-                print("Regulated virus top hit: FLAG")
-                reg_vir = 1
-            elif subset['superkingdom'][0] == "Bacteria": # if top hit that isn't a synthetic construct is regulated
-                print("Regulated bacteria top hit: FLAG")
-                reg_bac = 1
-            elif subset['superkingdom'][0] == "Fungi":
-                print("Regulated fungi top hit: FLAG")
-                reg_fung = 1
-        n_reg = blast['regulated'][blast['subject acc.'] == gene].sum()
-        n_total = len(blast['regulated'][blast['subject acc.'] == gene])
-        if (n_reg < n_total):
-            print("Gene " + gene + " found in both regulated and nonregulated organisms")
-            print("Species: " + " ".join(set(blast['species'][blast['subject acc.'] == gene])))
-        elif (n_reg == n_total):
-            print("Gene " + gene + " found in only regulated organisms")
-            print("Species: " + ", ".join(set(blast['species'][blast['subject acc.'] == gene])) + " (taxid: " + " ".join(map(str, set(blast['subject tax ids'][blast['subject acc.'] == gene]))) + ")")
-        else:
-            print("Gene: " + gene)
-            print(blast['regulated'][blast['subject acc.'] == gene])
+            n_reg = blast['regulated'][blast['subject acc.'] == gene].sum()
+            n_total = len(blast['regulated'][blast['subject acc.'] == gene])
+            # if some of the organisms with this gene aren't regulated, say so
+            if (n_reg < n_total):
+                print("Gene " + gene + " found in both regulated and non-regulated organisms")
+                print("Species: " + " ".join(set(blast['species'][blast['subject acc.'] == gene]))) # could explicitly list which are and aren't regulated?
+            # otherwise, raise a flag and say which superkingdom the flag belongs to
+            elif (n_reg == n_total):
+                if subset['superkingdom'][0] == "Viruses":
+                    reg_vir = 1
+                    org = "virus"
+                elif subset['superkingdom'][0] == "Bacteria": 
+                    reg_bac = 1
+                    org = "bacteria"
+                elif subset['kingdom'][0] == "Fungi":
+                    org = "fungi"
+                    reg_fung = 1
+                print(subset['superkingdom'][0])
+                print("Gene " + gene + " found in only regulated organisms: FLAG (" + org + ")")
+                print("Species: " + ", ".join(set(blast['species'][blast['subject acc.'] == gene])) + " (taxid: " + " ".join(map(str, set(blast['subject tax ids'][blast['subject acc.'] == gene]))) + ")")
+            else: # something is wrong, n_reg > n_total
+                print("Gene: " + gene)
+                print(blast['regulated'][blast['subject acc.'] == gene])
     hits = blast2[blast2['regulated']==True][['q. start', 'q. end']]  # print out the start and end coordinates of the query sequence
     hits.to_csv(sys.argv[1] + ".reg_path_coords.csv", index=False)
 
-if reg_bac == 0:
-    print("No regulated bacteria top hit: PASS")
-if reg_vir == 0:
-    print("No regulated virus top hit: PASS")
-if reg_fung == 0:
-    print("No regulated fungi top hit: PASS")
+if reg_vir == 0 and reg_bac == 0 and reg_fung == 0:
+    print("No regulated pathogen top hit: PASS")
 
 

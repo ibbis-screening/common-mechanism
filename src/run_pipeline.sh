92fc12940929243de3e78d2ec792fe1c49a4f1a6
Nicole Wheeler <nwheeler443@gmail.com>
2023-01-06 11:33:27 +0000

    fixed filename error

--- src/run_pipeline.sh
+++ src/run_pipeline.sh
@@ -154,7 +154,7 @@ fi
 
 # nucleotide screening
 echo " >> STEP 3: Checking regulated pathogen nucleotides..."
-python ${CM_DIR}/fetch_nc_bits_dmnd.py ${OUTPUT} ${QUERY}
+python ${CM_DIR}/fetch_nc_bits.py ${OUTPUT} ${QUERY}
 
 if [ -f "${OUTPUT}"_nc.fasta ]
 then blastn -query ${OUTPUT}_nc.fasta -db ${DB_PATH}/nt_blast/nt -out ${OUTPUT}.nt.blastn -outfmt "7 qacc stitle sacc staxids evalue bitscore pident qlen qstart qend slen sstart send" -max_target_seqs 50 -num_threads 8 -culling_limit 5 -evalue 10

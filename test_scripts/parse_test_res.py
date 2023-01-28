89cf672f82295115bd6e923d8163cf09ae47270f
Nicole Wheeler <nwheeler443@gmail.com>
2023-01-28 10:23:02 +0000

    Improve reporting in viz_outputs, parse_test_res

--- test_scripts/parse_test_res.py
+++ test_scripts/parse_test_res.py
@@ -102,18 +102,15 @@ for name, risk, reg_vir, reg_bac, reg_fungi, ben in breakdown:
         summary.append((name, "P"))
     # if it's a regulated bacterial hit, flag it
     elif reg_bac == "F":
-#        print("Nothing found")
         summary.append((name, "F"))
     elif reg_fungi == "F":
-#        print("Nothing found")
         summary.append((name, "F"))
     else:
-        summary.append((name, None))
-#print(summary)
+        summary.append((name, "P"))
 pd.DataFrame(summary).to_csv("test_summary.csv", index=False, header=None)
 
 breakdown = pd.DataFrame(breakdown)
-breakdown.columns = ("names", "biorisk", "regulated_virus", "regulated_bacteria", "regulated_fungi", "benign")
+breakdown.columns = ("names", "biorisk", "regulated_virus", "regulated_bacteria", "regulated_fungi/oomycetes", "benign")
 breakdown.to_csv("test_itemized.csv", index=False)
 
 

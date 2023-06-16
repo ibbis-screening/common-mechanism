fa6547b5a9e15a8d9cc95424e0cb0563ded4009c
Nicole Wheeler <nwheeler443@gmail.com>
2023-06-16 16:58:09 +0100

    adding pass flag

--- src/check_biorisk.py
+++ src/check_biorisk.py
@@ -63,7 +63,7 @@ def main():
                 sys.stdout.write("\t\t --> Biorisks: Regulated genes not found, PASS\n")
             if (sum(hmmer['Must flag']) != hmmer.shape[0]):
                 for region in hmmer.index[hmmer['Must flag'] == 0]:
-                    sys.stdout.write("\t\t --> Biorisks: Virulence factor found in bases " + str(hmmer['ali from'][region]) + " to " + str(hmmer['ali to'][region]) + ", WARNING\n\t\t     Gene: " + ", ".join(set(hmmer['description'][hmmer['Must flag'] == False])) + "\n")
+                    sys.stdout.write("\t\t --> Virulence factor found in bases " + str(hmmer['ali from'][region]) + " to " + str(hmmer['ali to'][region]) + ", WARNING\n\t\t     Gene: " + ", ".join(set(hmmer['description'][hmmer['Must flag'] == False])) + "\n")
         else: 
             sys.stdout.write("\t\t --> Biorisks: no significant hits detected, PASS\n")
     if res == 2:

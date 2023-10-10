7310781d75b46d5fce6e792cc4e6accfef01bb4e
Nicole Wheeler <nwheeler443@gmail.com>
2023-10-10 10:08:55 +0100

    noncoding troubleshooting

--- src/fetch_nc_bits.py
+++ src/fetch_nc_bits.py
@@ -67,7 +67,6 @@ def fetch_sequences(seqid, nc_bits, f_file, outfile):
                 for record in records:
                     if str(record.id) == str(seqid):
                         sequence = record.seq[start - 1 : stop]  # Adjust start to 0-based index
-                        print(sequence, record.seq)
                         sequences.append(f">{seqid} {start}-{stop}\n{sequence}\n")
                         break
                     else:

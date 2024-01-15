b6f6ff487ed2e3b78ecbbf2b70061154962e13c1
Nicole Wheeler <nwheeler443@gmail.com>
2024-01-15 15:48:15 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,16 +11,10 @@ setup(
     author = "Nicole Wheeler",
     author_email = "N.Wheeler@bham.ac.uk",
     description = ("An International Common Mechanism for DNA Synthesis Screening"),
-    license = "MIT",
     keywords = "",
-    url = "https://github.com/nwheeler443/CommonMechanism",
+    url = "https://github.com/DNA-Screening/CommonMechanism",
     packages = setuptools.find_packages(where="common_mechanism", exclude=("tests",)),
     package_data = { 'src/databases': ['*'] },
-    install_requires=["Biopython", "pandas", "taxoniq", "gdown", "plotly", "matplotlib", "kaleido", "pybedtools"],
+    install_requires=["Biopython", "pandas", "plotly", "matplotlib", "kaleido", "pybedtools", "pytaxonkit"],
     long_description=read('README.md'),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Scientific/Engineering :: Bio-Informatics"
-        "License :: OSI Approved :: MIT License",
-    ],
 )

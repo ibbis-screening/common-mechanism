ad173ad7b000ee9c35820365ba8339cddee7c4d3
Nicole Wheeler <nwheeler443@gmail.com>
2024-03-01 15:17:01 +0000

    python vs python3

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 ##############################################################################
 #utils.py provides functions used across multiple python scripts in the 

2a974bcd1deeda84b8c2b60ad2fec64cc3b87d69
Tessa Alexanian <hello@tessa.fyi>
2024-05-03 22:35:13 -0700

    first, most aggressively minimally functional toml

--- common-mechanism/split_query.py
+++ common-mechanism/split_query.py
@@ -7,19 +7,24 @@ import argparse
 import string
 from Bio import SeqIO
 
-parser = argparse.ArgumentParser(description="Split the fasta file into individual file with each gene seq")
-parser.add_argument('-f', action='store', dest='fasta_file', help='Input fasta file')
-result = parser.parse_args()
+VALID_FILENAME_CHARS = f"-.{string.ascii_letters}{string.digits}"
 
-with open(result.fasta_file, "r", encoding="utf-8") as input_file:
-    for record in SeqIO.parse(input_file, "fasta"):
-        desc = record.description
-        desc = desc.strip()
+def main():
+    parser = argparse.ArgumentParser(description="Split the fasta file into individual file with each gene seq")
+    parser.add_argument('-f', action='store', dest='fasta_file', help='Input fasta file')
+    result = parser.parse_args()
 
-        VALID_FILENAME_CHARS = f"-.{string.ascii_letters}{string.digits}"
-        FILENAME = "".join(x for x in desc if x in VALID_FILENAME_CHARS)
-        if len(FILENAME) > 150:
-            FILENAME = FILENAME[:150]
+    with open(result.fasta_file, "r", encoding="utf-8") as input_file:
+        for record in SeqIO.parse(input_file, "fasta"):
+            desc = record.description
+            desc = desc.strip()
 
-        with open(f"{FILENAME}.fasta", "w", encoding="utf-8") as output_file:
-            output_file.write(">"+str(desc)+"\n"+str(record.seq))
+            filename = "".join(x for x in desc if x in VALID_FILENAME_CHARS)
+            if len(filename) > 150:
+                filename = filename[:150]
+
+            with open(f"{filename}.fasta", "w", encoding="utf-8") as output_file:
+                output_file.write(">"+str(desc)+"\n"+str(record.seq))
+
+if __name__ == "__main__":
+    main()

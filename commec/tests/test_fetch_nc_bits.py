a0bc86966aafb46dc6c5b3e21226afe152dd948a
Tessa Alexanian <hello@tessa.fyi>
2024-09-20 21:16:29 -0700

    Add one more condition

--- commec/tests/test_fetch_nc_bits.py
+++ commec/tests/test_fetch_nc_bits.py
@@ -19,9 +19,10 @@ def create_mock_blast_df(hits):
     return df.reset_index(drop=True)  # This adds a numeric index
 
 @pytest.mark.parametrize("hits, nc_ranges", [
-    ([(1, 50), (100, 150), (175, 299)], []),  # Two protein hits, no gaps > 50bp
-    ([(50, 251)], []),                        # One protein hit, < 50bp nc on the ends
-    ([(51, 250)], [[1, 50], [251, 300]]),     # One protein hit, > 50bp nc on the ends
+    ([(1, 50), (100, 150), (175, 299)], []),            # Two protein hits, no gaps > 50bp
+    ([(50, 251)], []),                                  # One protein hit, < 50bp nc on the ends
+    ([(51, 250)], [[1, 50], [251, 300]]),               # One protein hit, > 50bp nc on the ends
+    ([(1, 40), (140, 265), (300, 349)], [[41, 139]])    # Three protein hits, one gap >50bp
 ])
 def test_get_ranges_with_no_hits(hits, nc_ranges):
     blast_df = create_mock_blast_df(hits)

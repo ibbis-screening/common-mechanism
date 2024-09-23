766b53cdfab41a3799d679941ea136d6065cb410
root <root@Michael-Laptop>
2024-09-23 14:04:35 +1200

    Additional database testing functionality, to check against pre-generated inputs, and also generating those inputs when necessary.

--- commec/tests/test_dbs.py
+++ commec/tests/test_dbs.py
@@ -13,6 +13,7 @@ from commec.tools.cmscan_handler import CmscanHandler
 INPUT_QUERY = os.path.join(os.path.dirname(__file__),"test_data/single_record.fasta")
 DATABASE_DIRECTORY = os.path.join(os.path.dirname(__file__),"test_dbs")
 
+# test each database type.
 databases_to_implement = [
     [DiamondHandler,   "nr_dmnd",     "nr.dmnd"],
     [BlastNHandler,    "nt_blast",    "nt"],
@@ -22,7 +23,7 @@ databases_to_implement = [
 ]
 
 @pytest.mark.parametrize("input_db", databases_to_implement)
-def test_database_can_run(input_db):
+def test_database_can_run(input_db, request):
     """
     Opens a database object on a test database, and runs the test query on it.
     Fails if commec environment is not setup correctly, or if the database object
@@ -32,10 +33,16 @@ def test_database_can_run(input_db):
     instead of --help during the conda recipe checks.
     """
 
+    create_exemplare : bool = request.config.getoption("--gen-examples")
     db_dir = os.path.join(DATABASE_DIRECTORY, input_db[1])
     db_file = os.path.join(db_dir, input_db[2])
-
+    exemplar_file = db_file+".exemplar.out"
     output_file = "db.out"
+
+    if create_exemplare:
+        db = input_db[0](db_file, INPUT_QUERY, exemplar_file)
+        db.screen()
+        assert db.check_output()
 
     new_db = input_db[0](db_file, INPUT_QUERY, output_file)
     new_db.screen()
@@ -43,6 +50,16 @@ def test_database_can_run(input_db):
 
     version : str = new_db.get_version_information()
     assert not version == "Version information retrieval error."
+
+    # Read output, and expected output, and compare line by line
+    with (open(output_file, 'r', encoding = "utf-8") as f1, 
+          open(exemplar_file, 'r', encoding = "utf-8") as f2):
+        for line1, line2 in zip(f1, f2):
+            if line1.startswith("# Option settings:"):
+                continue
+            if line1.startswith("# Date:"):
+                continue
+            assert line1 == line2
     
     if os.path.isfile(output_file):
         os.remove(output_file)

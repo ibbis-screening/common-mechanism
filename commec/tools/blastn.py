d3590a45ee4858ebec17e44d2befda5740b51ac8
Tessa Alexanian <hello@tessa.fyi>
2024-09-23 21:50:50 -0700

    Format blast search handlers, rewrite as format_args_for_cli

--- commec/tools/blastn.py
+++ commec/tools/blastn.py
@@ -1,49 +1,66 @@
 #!/usr/bin/env python3
 # Copyright (c) 2021-2024 International Biosecurity and Biosafety Initiative for Science
 """
-Module for a handlers, specifically for calling Blastn command line interface.
-Instantiate a BlastNHandler, with input local database, input fasta, and output file.
-Alter instants member variable arguments_dictionary with desired alternatives from the defaults.
-Throws if inputs are invalid. Creates a temporary log file, which is deleted on completion.
+Module for BLASTN handler, with input database, fasta to screen, and output file.
+Throws error if inputs are invalid. Creates a temporary log file, which is deleted on completion.
 """
 
 import subprocess
 from commec.tools.blast_tools import BlastHandler
 from commec.tools.search_handler import SearchToolVersion
 
+
 class BlastNHandler(BlastHandler):
-    """ 
-    A Database handler specifically for use with BlastX files for commec screening. 
-    Allows for full customization of any of the callable blast flags.
+    """
+    A search handler specifically for BLASTN command-line during commec screening.
+    Modify `arguments_dictionary` to change passed to the command line call.
     """
 
-    def __init__(self, database_file : str, input_file : str, out_file : str):
+    def __init__(self, database_file: str, input_file: str, out_file: str):
         super().__init__(database_file, input_file, out_file)
-        # We fill this with the defaults, however they can always be overridden after instancing, before screening.
+        # We fill this with defaults, however they can always be overridden before screening.
         self.arguments_dictionary = {
-            "-outfmt": ["7", "qacc",   "stitle",   "sacc",   "staxids",
-                        "evalue", "bitscore", "pident", "qlen",
-                        "qstart", "qend",     "slen",   "sstart",  "send"],
-                        "-num_threads": 8,
+            "-outfmt": [
+                "7",
+                "qacc",
+                "stitle",
+                "sacc",
+                "staxids",
+                "evalue",
+                "bitscore",
+                "pident",
+                "qlen",
+                "qstart",
+                "qend",
+                "slen",
+                "sstart",
+                "send",
+            ],
+            "-num_threads": 8,
             "-evalue": "10",
             "-max_target_seqs": 50,
-            "-culling_limit": 5
+            "-culling_limit": 5,
         }
         self.blastcall = "blastn"
 
     def search(self):
         command = [
             self.blastcall,
-            "-query", self.input_file,
-            "-db", self.db_file,
-            "-out", self.out_file
+            "-query",
+            self.input_file,
+            "-db",
+            self.db_file,
+            "-out",
+            self.out_file,
         ]
-        command.extend(self.get_arguments())
-        self.run_as_subprocess(command,self.temp_log_file)
+        command.extend(self.format_args_for_cli())
+        self.run_as_subprocess(command, self.temp_log_file)
 
     def get_version_information(self) -> SearchToolVersion:
         try:
-            result = subprocess.run(['blastn', '-version'], capture_output=True, text=True, check=True)
+            result = subprocess.run(
+                ["blastn", "-version"], capture_output=True, text=True, check=True
+            )
             version_info = result.stdout.strip()
             return version_info
         except subprocess.CalledProcessError:
--- commec/tools/blastx.py
+++ commec/tools/blastx.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 # Copyright (c) 2021-2024 International Biosecurity and Biosafety Initiative for Science
 """
-Module for a handlers, specifically for calling Blastx command line interface.
-Instantiate a BlastXHandler, with input local database, input fasta, and output file.
-Alter instants member variable arguments_dictionary with desired alternatives from the defaults.
-Throws if inputs are invalid. Creates a temporary log file, which is deleted on completion.
+Module for BLASTX handler, with input database, fasta to screen, and output file.
+Throws error if inputs are invalid. Creates a temporary log file, which is deleted on completion.
 """
 
 import subprocess
@@ -12,14 +10,14 @@ from commec.tools.blast_tools import BlastHandler
 from commec.tools.search_handler import SearchToolVersion
 
 class BlastXHandler(BlastHandler):
-    """ 
-    A Database handler specifically for use with BlastX files for commec screening. 
-    Allows for full customization of any of the callable blast flags.
+    """
+    A search handler specifically for BLASTX command-line during commec screening.
+    Modify `arguments_dictionary` to change passed to the command line call.
     """
 
-    def __init__(self, database_file : str, input_file : str, out_file : str):
+    def __init__(self, database_file: str, input_file: str, out_file: str):
         super().__init__(database_file, input_file, out_file)
-        # We fill this with the defaults, however they can always be overridden after instancing, before screening.
+        # We fill this with defaults, however they can always be overridden before screening.
         self.arguments_dictionary = {
             "-num_threads": 1,
             "-evalue": "1e-10",
@@ -32,25 +30,43 @@ class BlastXHandler(BlastHandler):
             "-gapopen": 11,
             "-gapextend": 1,
             "-seg": "yes",
-            "-outfmt": ["7", "qacc",   "stitle",   "sacc",   "staxids",
-                        "evalue", "bitscore", "pident", "qlen",
-                        "qstart", "qend",     "slen",   "sstart",  "send"]
+            "-outfmt": [
+                "7",
+                "qacc",
+                "stitle",
+                "sacc",
+                "staxids",
+                "evalue",
+                "bitscore",
+                "pident",
+                "qlen",
+                "qstart",
+                "qend",
+                "slen",
+                "sstart",
+                "send",
+            ],
         }
         self.blastcall = "blastx"
 
     def search(self):
         command = [
             self.blastcall,
-            "-db", self.db_file,
-            "-query", self.input_file,
-            "-out", self.out_file,
+            "-db",
+            self.db_file,
+            "-query",
+            self.input_file,
+            "-out",
+            self.out_file,
         ]
-        command.extend(self.get_arguments())
-        self.run_as_subprocess(command,self.temp_log_file)
+        command.extend(self.format_args_for_cli())
+        self.run_as_subprocess(command, self.temp_log_file)
 
     def get_version_information(self) -> SearchToolVersion:
         try:
-            result = subprocess.run(['blastx', '-version'], capture_output=True, text=True, check=True)
+            result = subprocess.run(
+                ["blastx", "-version"], capture_output=True, text=True, check=True
+            )
             version_info = result.stdout.splitlines()[0].strip()
             return version_info
         except subprocess.CalledProcessError:

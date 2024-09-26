810e984f3667351d71a57ddad356f7050470bdbe
Michael Barnett <ToMichaelBarnett@gmail.com>
2024-09-26 19:57:04 +1200

    Removed erroneous override for BlastHandler intermediary ABC.

--- commec/tools/blast_tools.py
+++ commec/tools/blast_tools.py
@@ -24,13 +24,6 @@ class BlastHandler(SearchHandler):
     A Database handler specifically for use with Blast. 
     Inherit from this, and implement screen()
     """
-    # Start Database Handler API
-    #@abstractmethod
-    #def search(self):
-    #    """
-    #    Use a tool to search the input query against a database.
-    #    Should be implemented by all subclasses to perform the actual search against the database.
-    #    """
 
     def _validate_db(self):
         """ 

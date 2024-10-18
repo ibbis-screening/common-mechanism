3c68d23e41d4328111cd9db340a7066811b25a04
Tessa Alexanian <hello@tessa.fyi>
2024-10-17 20:45:31 -0700

    less mock_open, more tmp_path

--- commec/tests/test_fetch_nc_bits.py
+++ commec/tests/test_fetch_nc_bits.py
@@ -1,8 +1,7 @@
-from unittest.mock import call, patch, mock_open
-import pytest
+from io import StringIO
 import os
 import pandas as pd
-from io import StringIO
+import pytest
 from Bio import SeqIO
 from commec.screeners.fetch_nc_bits import (
     get_ranges_with_no_hits,
@@ -41,33 +40,18 @@ def test_get_ranges_with_no_hits(hits, nc_ranges):
     assert get_ranges_with_no_hits(blast_df) == nc_ranges
 
 
-def test_write_nc_sequences():
-    nc_ranges = [[5, 59], [91, 170]]
+def test_write_nc_sequences(tmp_path):
     desc = "TEST_FETCH_NC"
     seq = "".join([str(i % 10) for i in range(300)])
     record = list(SeqIO.parse(StringIO(f">{desc}\n{seq}\n"), "fasta"))[0]
+    nc_ranges = [[5, 59], [91, 170]]
+    outfile = tmp_path / "output.fasta"
 
-    with patch(
-        "commec.screeners.fetch_nc_bits.open", new_callable=mock_open
-    ) as mock_file:
+    write_nc_sequences(nc_ranges, record, outfile)
 
-        write_nc_sequences(nc_ranges, record, "output.fasta")
-
-        mock_file.assert_any_call("output.fasta", "w", encoding="utf-8")
-        write_calls = (
-            mock_file.return_value.__enter__.return_value.writelines.call_args_list
-        )
-        assert (
-            len(write_calls) == 1
-        ), f"Expected 1 writelines call, but got {len(write_calls)}"
-
-        # not sure why the write_calls are at this weird triple index, don't @ me
-        sequences_written = write_calls[0][0][0]
-        expected_sequences = [
-            f">{desc} 5-59\n{seq[4:59]}\n",
-            f">{desc} 91-170\n{seq[90:170]}\n",
-        ]
-        assert expected_sequences == sequences_written
+    content = outfile.read_text()
+    assert f">{desc} 5-59\n{seq[4:59]}\n" in content
+    assert f">{desc} 91-170\n{seq[90:170]}\n" in content
 
 
 INPUT_FASTA = os.path.join(os.path.dirname(__file__), "test_data/fetch_nc_input.fasta")
@@ -80,7 +64,7 @@ EXPECTED_FILE = os.path.join(
 )
 
 
-def test_functional():
+def test_fetch_nocoding_regions():
     """
     Functional run of entire fetch_nc_bits, on a mock blast, and mock input fasta.
     """
